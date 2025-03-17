3b483304b929f80f02c1aed5bc8865250816a3ca
Михаил Капелько <kornerr@gmail.com>
2025-03-17 22:19:30 +0300

    d

---
+++ constants.js
@@ -0,0 +1,5 @@
+var CORS_PROXY = "https://vercel-cors-proxy-pi.vercel.app";
+var DIR = "/tutorial";
+var REPO_BRANCH = "main";
+var REPO_FILE = "README.md";
+var REPO_URL = "https://git.opengamestudio.org/kornerr/study-gitjs-access";
