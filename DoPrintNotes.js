ff41b6f1ed4d6ecb4250af3e4001fcc7e071c4d6
Михаил Капелько <kornerr@gmail.com>
2025-04-17 22:15:42 +0300

    d

--- DoPrintNotes.js
+++ DoPrintNotes.js
@@ -7,9 +7,18 @@ function DoPrintNotes() {
 
     this.printNotes = async function() {
         console.log("ИГР DoPN.printN-1")
-        var contentConfig = await pfs.readFile(FILE_CONFIG, {encoding: "utf8"});
+        var files = await pfs.readdir(DIR);
+
+        var contentConfig = "(null)";
+        if (files.includes(FILE_CONFIG_REL)) {
+            contentConfig = await pfs.readFile(FILE_CONFIG, {encoding: "utf8"});
+        }
         console.log("ИГР DoPN.print-2 config:", contentConfig);
-        var contentLog = await pfs.readFile(FILE_LOG, {encoding: "utf8"});
+
+        var contentLog = "(null)";
+        if (files.includes(FILE_LOG_REL)) {
+            contentLog = await pfs.readFile(FILE_LOG, {encoding: "utf8"});
+        }
         console.log("ИГР DoPN.print-3 log:", contentLog);
     };
 
