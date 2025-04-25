49dfe00d581f4b283e3f3f3addd9ebd1b9c1d161
Михаил Капелько <kornerr@gmail.com>
2025-04-25 22:35:26 +0300

    d

--- DoFetchReadOnlyBranches.js
+++ DoFetchReadOnlyBranches.js
@@ -1,13 +1,31 @@
 function DoFetchReadOnlyBranches() {
     this._construct = function() {
+        this.repoURL = null;
         this.rootFiles = [];
         this.schedule = null;
     };
     this._construct();
 
     this.execute = async function() {
-        await this.resetRootFiles();
         console.log("ИГР DoFROB.execute");
+        await this.resetRootFiles();
+        await this.readRepoURL();
+    };
+
+    this.readRepoURL = async function() {
+        if (!this.rootFiles.includes(DIR_REL)) {
+            return;
+        }
+        console.log("ИГР DoFROB.readRU-01");
+        var remotes = await git.listRemotes({ dir: DIR });
+        for (var i in remotes) {
+            var item = remotes[i];
+            if (item.remote == ORIGIN) {
+                this.repoURL = item.url;
+                break;
+            }
+        }
+        console.log("ИГР DoFROB.readRU-02 repoU:", this.repoURL);
     };
 
     this.resetRootFiles = async function() {
