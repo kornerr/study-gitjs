f13119d5edf6319d0b928e192929ded12045519d
Михаил Капелько <kornerr@gmail.com>
2025-04-28 22:46:29 +0300

    d

--- DoDisplayNotes.js
+++ DoDisplayNotes.js
@@ -5,26 +5,20 @@ function DoDisplayNotes(
     this._construct = function() {
         this.elForm = elForm;
         this.elSection = elSection;
+        this.currentNotes = [];
+        this.dirFiles = [];
         this.rootFiles = [];
         this.schedule = null;
     };
     this._construct();
 
     this.displayNotes = async function() {
-        if (!this.rootFiles.includes(DIR_REL)) {
+        if (this.currentNotes.length == 0) {
             return;
         }
-        console.log("ИГР DoDN.displayN-1")
-        var files = await pfs.readdir(DIR);
-
-        var contentLog = "";
-        if (files.includes(FILE_LOG_REL)) {
-            contentLog = await pfs.readFile(FILE_LOG, {encoding: "utf8"});
-        }
-        var notes = parseNotes(contentLog);
-        console.log("ИГР DoDN.displayN-2 notes:", notes);
+        console.log("ИГР DoDN.displayN-1 notes");
         var html = "";
-        for (var i in notes) {
+        for (var i in this.currentNotes) {
             var n = notes[i];
             html += FMT_NOTE
                 .replaceAll("%DATE%", formatDate(n.date))
@@ -35,11 +29,25 @@ function DoDisplayNotes(
     };
 
     this.execute = async function() {
-        await this.resetRootFiles();
-        await this.displayNotes();
+        await this.resetFiles();
+        await this.readCurrentNotes();
+        //await this.readOtherNotes();
+        this.displayNotes();
     };
 
-    this.resetRootFiles = async function() {
-        this.rootFiles = await pfs.readdir("/");
+    this.readCurrentNotes = async function() {
+        if (!this.dirFiles.includes(FILE_LOG_REL)) {
+            return;
+        }
+        console.log("ИГР DoDN.readCN-1")
+        var contentLog = await pfs.readFile(FILE_LOG, {encoding: "utf8"});
+        this.currentNotes = parseNotes(contentLog);
+    };
+
+    this.resetFiles = async function() {
+        this.rootFiles = await pfs.readdir(ROOT);
+        if (this.rootFiles.includes(DIR_REL)) {
+            this.dirFiles = await pfs.readdir(DIR);
+        }
     };
 }
